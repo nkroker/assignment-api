8aa23d01d2c9bde6dc3074c31b8072752fe1f342
nkroker <nikhilq.75@gmail.com>
2024-10-18 15:39:42 +0530

    added bycrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,5 @@ class User < ApplicationRecord
   has_many :task_assignments
   has_many :tasks, through: :task_assignments
 
-  before_action :encrypt_password
-
-  def encrypt_password
-  end
+  has_secure_password
 end
